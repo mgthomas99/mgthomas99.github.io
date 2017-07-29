f56e6b2fcbe0751671d61ec75d5ed2c53e2972ec
mgthomas99 <m.g.thomas99@gmail.com>
2017-07-29 19:02:17 +0100

    commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const server = express();
+server.use(express.static("static/"));
+server.get("/", (req, res) => {
+    res.sendFile("static/index.html");
+});
+server.get("/:r", (req, res) => {
+    res.send(String(Math.random()));
+});
+server.listen(8080);
+console.log("Server listening on port 8080!");
