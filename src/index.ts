f0665777f684e05ad5f0a21caba80aa4001f7762
George Thomas <mgthomas99@users.noreply.github.com>
2017-07-29 19:00:42 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ server.use(express.static("static/"))
 server.get("/", (req: Request, res: Response) => {
     res.sendFile("static/index.html");
 });
+server.get("/:r", (req: Request, res: Response) => {
+    res.send(String(Math.random()));
+});
 
 server.listen(8080);
 console.log("Server listening on port 8080!");
